1c958a4a75be60d7bc31f3a8b8bceefe2a7f2517
physcocode <pranjaltech10@gmail.com>
2021-01-25 16:30:12 +0530

    feat: welcome screen

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Scrawler PHP</title>
+    <title>OSSNAPI Server</title>
     <style>
         h1 {
             font-size: 40px;
@@ -25,7 +25,7 @@
         }
 
         a{
-            color: #000;
+            color:#2554ca;
             text-decoration: none;
             font-size: 20px;
         }
@@ -34,13 +34,22 @@
 
 <body>
     <div class="center">
-        <h1> Welcome to Scrawler Framework</h1>
+        <h1 > Welcome to OSSNAPI Server!</h1>
         <center>
-            <a href="https://github.com/scrawler-php/scrawler">Github</a>
-            &nbsp;
-            <a href="https://scrawler.io">Website</a> 
+            <a href="https://github.com/OSSNAPI/OpenAPI-spec">OSSNAPI Spec</a>
+            &nbsp;&nbsp;&nbsp;
+            <a href="https://github.com/OSSNAPI/OSSN-server-php">Github</a>
+            &nbsp;&nbsp;&nbsp;
+            <a href="https://www.postman.com/pranjalshop/workspace/the-social-network-api-s/overview">Postman Workspace</a>
+            &nbsp;&nbsp;&nbsp;
+            <a href="https://physcocode.com/category/postman/">Blog</a>
+
         </center>
+        <br>
+
+
     </div>
+
 </body>
 
 </html>
